15bbe9edfc0e7f3f3e75adefe54a998b93a22862
Rose Yu <qiyu@usc.edu>
2018-10-30 21:35:08 -0400

    added HW4

---
+++ CS6140/HW4/prob2utils_skeleton.py
@@ -0,0 +1,56 @@
+# Solution routine for matrix factorization
+# CSCI 6140 HW4
+
+
+import numpy as np
+
+def grad_U(Ui, Yij, Vj, reg, eta):
+    """
+    Takes as input Ui (the ith row of U), a training point Yij, the column
+    vector Vj (jth column of V^T), reg (the regularization parameter lambda),
+    and eta (the learning rate).
+
+    Returns the gradient of the regularized loss function with
+    respect to Ui multiplied by eta.
+    """
+    pass   
+
+def grad_V(Vj, Yij, Ui, reg, eta):
+    """
+    Takes as input the column vector Vj (jth column of V^T), a training point Yij,
+    Ui (the ith row of U), reg (the regularization parameter lambda),
+    and eta (the learning rate).
+
+    Returns the gradient of the regularized loss function with
+    respect to Vj multiplied by eta.
+    """
+    pass
+
+def get_err(U, V, Y, reg=0.0):
+    """
+    Takes as input a matrix Y of triples (i, j, Y_ij) where i is the index of a user,
+    j is the index of a movie, and Y_ij is user i's rating of movie j and
+    user/movie matrices U and V.
+
+    Returns the mean regularized squared-error of predictions made by
+    estimating Y_{ij} as the dot product of the ith row of U and the jth column of V^T.
+    """
+    pass
+
+
+def train_model(M, N, K, eta, reg, Y, eps=0.0001, max_epochs=300):
+    """
+    Given a training data matrix Y containing rows (i, j, Y_ij)
+    where Y_ij is user i's rating on movie j, learns an
+    M x K matrix U and N x K matrix V such that rating Y_ij is approximated
+    by (UV^T)_ij.
+
+    Uses a learning rate of <eta> and regularization of <reg>. Stops after
+    <max_epochs> epochs, or once the magnitude of the decrease in regularized
+    MSE between epochs is smaller than a fraction <eps> of the decrease in
+    MSE after the first epoch.
+
+    Returns a tuple (U, V, err) consisting of U, V, and the unregularized MSE
+    of the model.
+    """
+    pass
